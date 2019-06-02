8e3b98f1cf3dd9dc6853e09d7d7de83268ffc9c1
Puhong Duan <49089403+PuhongDuan@users.noreply.github.com>
2019-06-02 11:47:33 +0800

    Update average_fusion.m

--- functions/average_fusion.m
+++ functions/average_fusion.m
@@ -4,7 +4,7 @@ function [ R ] = average_fusion( img,n )
 no_bands=size(img,3);
 for i=1:n
 R(:,:,i)= mean(img(:,:,1+floor(no_bands/n)*(i-1):floor(no_bands/n)*i),3);
-if (floor(no_bands/n)*i~=no_bands)&(i==n)%当不够等分时，剩下的算作一类。。
+if (floor(no_bands/n)*i~=no_bands)&(i==n)%
     R(:,:,i+1)=mean(img(:,:,0+floor(no_bands/n)*i:no_bands),3);
 end    
 end
